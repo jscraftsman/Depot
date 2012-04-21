6d8dfd0a897bf7554dab68de359e3f191c4a9742
Gervel Giva <gervelgiva@gmail.com>
2012-04-21 19:42:39 +0800

    Atom not working

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,10 +1,9 @@
 class LineItem < ActiveRecord::Base
   attr_accessible :cart_id, :product_id, :product, :id, :created_at, :updated_at, :quantity, :quantitiy
+  belongs_to :order
   belongs_to :product
   belongs_to :cart
-
   def total_price
     product.price * quantity
   end
- 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,25 @@
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 Depot::Application.routes.draw do
+  resources :orders
+
   resources :line_items
 
   resources :carts
 
   get "store/index"
 
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
 
+  # ...
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -57,11 +70,12 @@ Depot::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-  root :to => 'store#index', :as => 'store'
+  root to: 'store#index', as: 'store'
+  # ...
 
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  # match ':controller(/:action(/:id(.:format)))'
 end
